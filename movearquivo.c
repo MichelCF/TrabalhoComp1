2d35df8c3e6f750c14d37bfb828402eb562c1648
tequinho <matheusmart95@gmail.com>
2016-12-30 22:28:03 -0200

    Add files via upload

---
+++ movearquivo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int move(FILE *arq){
+	int matriz[9][9];
+	int i,j;
+	fseek(arq,162,SEEK_SET);
+	for(i=0;i<9;i++){
+	    for(j=0;j<9;j++){
+		fscanf(arq,"%i",&matriz[i][j]);
+	    }
+	}
+        for(i=0;i<9;i++){
+		for(j=0;j<9;j++){
+		printf("%i ",matriz[i][j]);
+	         }
+	printf("\n");
+        }
+}	
+
